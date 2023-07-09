4761444aa8c532a63d5fd9fc862b23b53e570e8e
vovanthem <vovanthemk35tt@gmail.com>
2023-07-09 17:55:01 +0700

    feat: remove value unused

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import Logo from "images/logo.png";
 import { Link } from "react-router-dom";
